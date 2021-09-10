7ea17237924764441de1dff991bdb064b739f5e3
Shahin <shahin.zeynalov95@gmail.com>
2021-09-10 18:47:55 +0400

    Data added , play&pause functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Song from './components/Song'
+import Player from "./components/Player";
+
+//Styles
+import './styles/app.scss';
+//Util
+import data from './util'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [songs , setSongs] = useState(data());
+    const [currentSong , setCurrentSong] = useState(songs[0]);
+    const [isPlaying , setIsPlaying] = useState(false)
+
+    return (
+        <div className="App">
+            <Song currentSong={currentSong}/>
+            <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+        </div>
+    );
 }
 
 export default App;
